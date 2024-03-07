ed3ee6f1013a99ded9c0bbc658f35de5dbf3697f
Vandlol <max@henzemail.org>
2024-03-07 23:21:32 +0100

    more

--- helper/files.py
+++ helper/files.py
@@ -3,9 +3,22 @@ import os
 
 pyphoria_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 def create_path_and_file(file_name: str) -> None:
     if not os.path.exists(f"{pyphoria_path}/{os.path.dirname(file_name)}"):
         os.makedirs(f"{pyphoria_path}/{os.path.dirname(file_name)}")
-    open(f"{pyphoria_path}/{file_name}", 'w').close()
+        open(f"{pyphoria_path}/{file_name}", "w").close()
 
 
+def read_json(file_name: str) -> dict:
+    with open(f"{pyphoria_path}/{file_name}", "r") as file:
+        try:
+            data = json.load(file)
+        except json.decoder.JSONDecodeError:
+            data = {}
+    return data
+
+
+def write_json(file_name: str, data: dict) -> None:
+    with open(f"{pyphoria_path}/{file_name}", "w") as file:
+        json.dump(data, file, indent=4)
--- models/Character.py
+++ models/Character.py
@@ -1,8 +1,55 @@
 import helper.files
+import uuid
 
-class Character:
-    def __init__(self) -> None:
-        self.data_file_name = 'data/characters.json'
+from typing import Self
+from pprint import pprint as pp
 
-        self.data_file_handle = open(self.data_file_name, 'r')
-        self.data = json.load(self.data_file_handle)
+
+# from pydantic import BaseModel, PositiveInt
+
+# class CharacterModel(BaseModel):
+#     id: uuid.UUID
+#     name: str
+#     surname: str
+#     level: PositiveInt
+#     experience: PositiveInt
+#     energy: PositiveInt
+#     gold: PositiveInt
+#     inventory: list
+
+
+class Model:
+    def __init__(self: Self, account_id: uuid) -> None:
+        self.account_id: uuid = account_id
+        self.data_file_name: str = f"data/characters-{self.account_id}.json"
+        helper.files.create_path_and_file(self.data_file_name)
+        self.data = helper.files.read_json(self.data_file_name)
+
+    def check_name_available(self: Self, name: str, surname: str) -> bool:
+        for character_id, character in self.data.items():
+            if f"{character['name']} {character['surname']}" == f"{name} {surname}":
+                return False
+        return True
+
+    def create(self: Self, name: str, surname: str) -> None:
+        if not self.check_name_available(name, surname):
+            print("Name already taken.")
+            return
+        character_id = str(uuid.uuid4())
+        self.data[character_id] = {
+            "id": character_id,
+            "name": name,
+            "surname": surname,
+            "level": 1,
+            "experience": 0,
+            "energy": 10,
+            "gold": 0,
+            "inventory": [],
+        }
+        self.save()
+
+    def load(self: Self, character_id: uuid) -> dict:
+        return self.data[character_id]
+
+    def save(self: Self) -> None:
+        helper.files.write_json(self.data_file_name, self.data)
