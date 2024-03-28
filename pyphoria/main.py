c4edf1eda40e0c873415b779c22f75d53130c081
Oluf Lorenzen <finkregh@mafia-server.net>
2024-03-29 00:57:42 +0100

    stuff

--- pyphoria/main.py
+++ pyphoria/main.py
@@ -5,10 +5,23 @@ import uuid
 from collections.abc import Sequence
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from fastapi.exceptions import ResponseValidationError
+from pydantic import PositiveInt
+from sqlalchemy import Engine
 from sqlmodel import Session, SQLModel, create_engine, select
 
-from pyphoria.models import Character, Inventory, InventoryItemLink, Item
+from pyphoria.models import (
+    Character,
+    CharacterCreate,
+    CharacterRead,
+    HTTPError,
+    Inventory,
+    InventoryItemLink,
+    Item,
+    Species,
+    SpeciesRead,
+)
 
 SQLITE_DB = "database.db"
 sqlite_url = f"sqlite:///{SQLITE_DB}"
@@ -19,11 +32,11 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=False, connect_args=connect_args)
+engine: Engine = create_engine(sqlite_url, echo=False, connect_args=connect_args)
+
 
 def create_db_and_tables():  # noqa: ANN201, D103
     SQLModel.metadata.create_all(engine)
-
 
 
 @asynccontextmanager
@@ -31,31 +44,57 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
-@app.post("/character/")
-def create_character(character: Character) -> Character:
+
+@app.get("/species/")
+async def get_species() -> Sequence[SpeciesRead]:
     with Session(engine) as session:
-        session.add(character)
-        session.commit()
-        session.refresh(character)
-        return character
+        species = session.exec(select(Species)).all()
+        return species
 
 
-@app.get("/character/")
-def get_characters() -> Sequence[Character]:
+@app.get("/character/", response_model=list[CharacterRead])
+async def get_characters() -> Sequence[CharacterRead]:
     with Session(engine) as session:
         characters = session.exec(select(Character)).all()
         return characters
 
-@app.get("/character/{character_id}")
-def get_character(character_id: uuid.UUID) -> Character | None:
+
+@app.post("/character/", response_model=CharacterRead)
+async def create_character(character: CharacterCreate) -> CharacterCreate:
     with Session(engine) as session:
-        character = session.get(Character, character_id)
-        return character
+        db_character: CharacterCreate = CharacterCreate.model_validate(character)
+        session.add(db_character)
+        session.commit()
+        session.refresh(db_character)
+        return db_character
+
+
+@app.get("/character/{character_id}")
+async def get_character(character_id: uuid.UUID) -> CharacterRead:
+    with Session(engine) as session:
+        try:
+            character = session.get(Character, character_id)
+            return character
+        except ResponseValidationError:
+            return HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Character not found",
+            )
+        if not character:
+            return HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Character not found",
+            )
+
 
 @app.put("/character/{character_id}")
-def update_character(character_id: uuid.UUID, character: Character) -> Character:
+async def update_character(
+    character_id: uuid.UUID,
+    character: CharacterRead,
+) -> Character:
     with Session(engine) as session:
         character = session.get(Character, character_id)
         character.update(character)
@@ -65,40 +104,168 @@ def update_character(character_id: uuid.UUID, character: Character) -> Character
         return character
 
 
-@app.get("/character/{character_id}/inventory")
-def get_inventory(character_id: uuid.UUID) -> tuple[Inventory, list[Item] | None] | None:
+@app.get(
+    "/character/{character_id}/inventory",
+    responses={
+        # 200: {"model": Dummy},
+        404: {
+            "model": HTTPError,
+            "description": "Character not found",
+        },
+    },
+)
+async def get_character_inventory(
+    character_id: uuid.UUID,
+) -> Inventory | None:
     with Session(engine) as session:
         character = session.get(Character, character_id)
-        if character is None:
-            return None
-        items_ids: Sequence[uuid.UUID] = session.exec(select(InventoryItemLink.item_id).where(InventoryItemLink.inventory_id == character.inventory_id)).all()
-        inventory: Inventory | None =  session.get(Inventory, character.inventory_id)
-
-        if not items_ids:
-            return (inventory, None)
-
-        items: list[Item] = []
-        for item_id in items_ids:
-            item = session.get(Item, item_id)
-            items.append(item)
-        return (inventory, items)
+        if not character:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND,
+                detail="Character not found",
+            )
+        return session.get(Inventory, character.inventory_id)
 
 
 @app.get("/inventory/")
-def get_inventories() -> Sequence[Inventory]:
+async def get_inventories() -> Sequence[Inventory]:
     with Session(engine) as session:
         inventories = session.exec(select(Inventory)).all()
         return inventories
 
+
 @app.get("/inventory/{inventory_id}")
-def get_inventory(inventory_id: uuid.UUID) -> tuple[Inventory, list[Item] | None] | None:
+async def get_inventory(
+    inventory_id: uuid.UUID,
+) -> Inventory | None:
+    with Session(engine) as session:
+        return session.get(Inventory, inventory_id)
+
+
+@app.post("/inventory/")
+async def create_inventory(inventory: Inventory) -> Inventory:
+    with Session(engine) as session:
+        session.add(inventory)
+        session.commit()
+        session.refresh(inventory)
+        return inventory
+
+
+@app.put("/inventory/{inventory_id}")
+async def update_inventory(inventory_id: uuid.UUID, inventory: Inventory) -> Inventory:
     with Session(engine) as session:
         inventory = session.get(Inventory, inventory_id)
-        item_ids = session.exec(select(InventoryItemLink.item_id).where(InventoryItemLink.inventory_id == inventory_id)).all()
-        if not item_ids:
-            return (inventory, None)
-        items: list[Item] = []
-        for item_id in item_ids:
-            item: Item | None = session.get(Item, item_id)
-            items.append(item)
-        return (inventory, items)
+        inventory.update(inventory)
+        session.add(inventory)
+        session.commit()
+        session.refresh(inventory)
+        return inventory
+
+
+@app.delete("/inventory/{inventory_id}")
+async def delete_inventory(inventory_id: uuid.UUID) -> Inventory | None:
+    with Session(engine) as session:
+        inventory: Inventory | None = session.get(Inventory, inventory_id)
+        if not inventory:
+            return None
+        character: Character | None = session.exec(
+            select(Character).where(Character.inventory_id == inventory_id),
+        ).one_or_none()
+        if character:
+            character.inventory_id = None
+            session.add(character)
+            session.commit()
+        session.delete(inventory)
+        session.commit()
+        return inventory
+
+
+@app.post("/item/")
+async def create_item(item: Item) -> Item:
+    with Session(engine) as session:
+        session.add(item)
+        session.commit()
+        session.refresh(item)
+        return item
+
+
+@app.get("/item/")
+async def get_items() -> Sequence[Item]:
+    with Session(engine) as session:
+        items: Sequence[Item] = session.exec(select(Item)).all()
+        return items
+
+
+@app.get("/item/{item_id}")
+async def get_item(item_id: uuid.UUID) -> Item | None:
+    with Session(engine) as session:
+        item: Item | None = session.get(Item, item_id)
+        return item
+
+
+@app.put("/item/{item_id}")
+async def update_item(item_id: uuid.UUID, item: Item) -> Item:
+    with Session(engine) as session:
+        item = session.get(Item, item_id)
+        item.update(item)
+        session.add(item)
+        session.commit()
+        session.refresh(item)
+        return item
+
+
+@app.delete("/item/{item_id}")
+async def delete_item(item_id: uuid.UUID) -> Item:
+    with Session(engine) as session:
+        item: Item | None = session.get(Item, item_id)
+        if not item:
+            log.info("Item %s not found", item_id)
+            return None
+        inventories_with_item: Sequence[InventoryItemLink] = session.exec(
+            select(InventoryItemLink).where(InventoryItemLink.item_id == item_id),
+        ).all()
+        for inventory in inventories_with_item:
+            log.info(
+                "Removing item %s from inventory %s",
+                item_id,
+                inventory.inventory_id,
+            )
+            _inventory: Inventory | None = session.get(
+                Inventory,
+                inventory.inventory_id,
+            )
+            if _inventory:
+                _inventory.items.remove(item_id)
+                session.add(_inventory)
+
+        log.info("Deleting item %s", item_id)
+        session.delete(item)
+        session.commit()
+        return item
+
+
+@app.post("/inventory/{inventory_id}/item/{item_id}/{item_count}")
+async def add_item_to_inventory(
+    inventory_id: uuid.UUID,
+    item_id: uuid.UUID,
+    item_count: PositiveInt,
+) -> Inventory:
+    with Session(engine) as session:
+        inventory: Inventory | None = session.get(Inventory, inventory_id)
+        if not inventory:
+            return ValueError(f"Inventory with id {inventory_id} not found")
+        item: Item | None = session.get(Item, item_id)
+        if not item:
+            return ValueError(f"Item with id {item_id} not found")
+        if item_id in inventory.items:
+            return ValueError(f"Item with id {item_id} already in inventory")
+        inventory.items.append(item_id)
+        session.add(inventory)
+        inventory_item_link = InventoryItemLink(
+            inventory_id=inventory_id,
+            item_id=item_id,
+            item_count=item_count,
+        )
+        session.add(inventory_item_link)
+        session.commit()
+        return inventory
--- pyphoria/models/__init__.py
+++ pyphoria/models/__init__.py
@@ -2,7 +2,9 @@ import uuid
 from typing import TYPE_CHECKING
 
 from pydantic import (
+    BaseModel,
     ConfigDict,
+    NonNegativeInt,
     PositiveInt,
 )
 from sqlmodel import Field, Relationship, SQLModel
@@ -16,7 +18,22 @@ class StrictBaseModel(SQLModel):  # noqa: D101
         strict = True
 
 
-class CharacterStats(SQLModel, table=False):  # noqa: D101
+class Dummy(BaseModel):
+    name: str
+
+
+class HTTPError(BaseModel):
+    detail: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {"detail": "HTTPException raised."},
+        }
+
+
+class CharacterStats(SQLModel):
+    """Character stats model."""
+
     stat_combat: PositiveInt
     stat_dodge_rating: PositiveInt
     stat_hit_rating: PositiveInt
@@ -25,13 +42,22 @@ class CharacterStats(SQLModel, table=False):  # noqa: D101
     stat_vitality: PositiveInt
 
 
-class Species(CharacterStats, table=True):  # noqa: D101
-    id: uuid.UUID = Field(
-        default_factory=uuid.uuid4,
-        primary_key=True,
-        index=True,
-        nullable=False,
-    )
+class SpeciesBase(CharacterStats):
+    """
+    Species base model.
+
+    Inherit from CharacterStats to have the same stats in Species and Character.
+
+    Attributes
+        name (str): The name of the species.
+        description (str): The description of the species.
+        icon (str): The icon of the species.
+        base_strength (PositiveInt): The base strength of the species.
+        base_dexterity (PositiveInt): The base dexterity of the species.
+        base_intelligence (PositiveInt): The base intelligence of the species.
+
+    """
+
     name: str = Field(default=None, unique=True, index=True)
     description: str = Field(default=None)
     icon: str
@@ -40,26 +66,80 @@ class Species(CharacterStats, table=True):  # noqa: D101
     base_intelligence: PositiveInt
 
 
-class Character(CharacterStats, table=True):  # noqa: D101
-    # id: Uuid = Field(default_factory=uuid.uuid4, primary_key=True, unique=True)
+class Species(SpeciesBase, table=True):
+    """Species model."""
+
+    id: uuid.UUID | None = Field(
+        default_factory=uuid.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
+
+
+class SpeciesCreate(SpeciesBase):
+    """Species create model."""
+
+
+class SpeciesRead(SpeciesBase):
+    """Species read model."""
+
+    id: uuid.UUID
+
+
+class CharacterBase(CharacterStats):
+    """
+    Character base model.
+
+    Inherit from CharacterStats to have the same stats in Species and Character.
+
+    Attributes
+        species (uuid.UUID): The species of the character.
+        name (str): The name of the character.
+        surname (str): The surname of the character.
+        level (PositiveInt): The level of the character.
+        experience (PositiveInt): The experience of the character.
+        energy (PositiveInt): The energy of the character.
+        inventory_id (uuid.UUID): The inventory ID of the character.
+        strength (PositiveInt): The strength of the character.
+        dexterity (PositiveInt): The dexterity of the character.
+        intelligence (PositiveInt): The intelligence of the character.
+
+    """
+
+    # account_id: uuid.UUID = Field(default=None, foreign_key="accounts.id")  # noqa: ERA001
+    species: uuid.UUID = Field(foreign_key="species.id")
+    name: str
+    surname: str
+    level: PositiveInt
+    experience: PositiveInt
+    energy: PositiveInt
+    strength: PositiveInt
+    dexterity: PositiveInt
+    intelligence: PositiveInt
+
+
+class Character(CharacterBase, table=True):  # noqa: D101
     id: uuid.UUID = Field(
         default_factory=uuid.uuid4,
         primary_key=True,
         index=True,
         nullable=False,
     )
-    # account_id: uuid.UUID = Field(default=None, foreign_key="accounts.id")  # noqa: ERA001
-    species: uuid.UUID = Field(foreign_key="species.id")
-    name: str = Field(max_length=50, unique=True, index=True)
-    surname: str
-    level: PositiveInt = 1
-    experience: PositiveInt = 0
-    energy: PositiveInt
     inventory_id: uuid.UUID = Field(foreign_key="inventory.id")
-    # inventory: Inventory | None = Relationship(back_populates="character_id")
-    strength: PositiveInt
-    dexterity: PositiveInt
-    intelligence: PositiveInt
+
+
+class CharacterCreate(CharacterBase):
+    """Character create model."""
+
+    # inventory_id: uuid.UUID | None = None
+
+
+class CharacterRead(CharacterBase):
+    """Character read model."""
+
+    id: uuid.UUID
+    inventory_id: uuid.UUID
 
 
 class InventoryExtension(SQLModel, table=True):  # noqa: D101
@@ -73,21 +153,37 @@ class InventoryExtension(SQLModel, table=True):  # noqa: D101
     description: str
     icon: str
 
+
 class InventoryItemLink(SQLModel, table=True):
     inventory_id: uuid.UUID = Field(foreign_key="inventory.id", primary_key=True)
     item_id: uuid.UUID = Field(foreign_key="item.id", primary_key=True)
+    item_count: PositiveInt
 
 
-class Inventory(SQLModel, table=True):  # noqa: D101
+class InventoryBase(SQLModel):  # noqa: D101
+    slots: NonNegativeInt = Field(default=10)
+    gold: NonNegativeInt = Field(default=0)
+
+
+class InventoryCreate(InventoryBase):  # noqa: D101
+    pass
+
+
+class InventoryRead(InventoryBase):  # noqa: D101
+    id: uuid.UUID
+    items: list["Item"]
+
+
+class Inventory(InventoryBase, table=True):  # noqa: D101
     id: uuid.UUID = Field(
         default_factory=uuid.uuid4,
         primary_key=True,
         unique=True,
     )
-    slots: PositiveInt
-    gold: PositiveInt
     items: list["Item"] = Relationship(
-       back_populates="inventories", link_model=InventoryItemLink,
+        back_populates="inventories",
+        link_model=InventoryItemLink,
+        sa_relationship_kwargs={"lazy": "selectin"},
     )
     # extensions: List["InventoryExtension"] = Relationship(
     #    back_populates="inventory",
@@ -117,7 +213,10 @@ class Item(ItemDamage, ItemRequirements, table=True):  # noqa: D101
     unique_store: bool
     unique_equipped: bool
     icon: str
-    inventories: list["Inventory"] = Relationship(back_populates="items", link_model=InventoryItemLink)
+    inventories: list["Inventory"] = Relationship(
+        back_populates="items",
+        link_model=InventoryItemLink,
+    )
 
 
 class Monster(SQLModel, table=True):  # noqa: D101
